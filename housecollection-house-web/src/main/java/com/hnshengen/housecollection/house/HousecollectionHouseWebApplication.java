f0ae9bd5b81e03699d78779788c7121eac71c731
yzy <yyyy>
2019-12-20 09:12:12 +0800

    初始push

---
+++ housecollection-house-web/src/main/java/com/hnshengen/housecollection/house/HousecollectionHouseWebApplication.java
@@ -0,0 +1,13 @@
+package com.hnshengen.housecollection.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousecollectionHouseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousecollectionHouseWebApplication.class, args);
+	}
+
+}
